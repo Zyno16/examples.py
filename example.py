128131ae39a97408991636afe34bcc4a932afc18
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-10 18:24:59 +0000

    Add files via upload

---
+++ example.py
@@ -0,0 +1,12 @@
+friends=["zino","yusra","ines"]
+for friend in friends:
+    print("ramadan mubarek:",friend)
+print(friends[0])
+#this example explain we can change 
+# the value  if integent inside the 
+# list but string we cn not
+lotto=[0,1,3,4,6,7,"read",9]
+lotto[2] =(58)
+print (lotto)
+#on list read is equal to 1 in lengh
+print(len(lotto)) 
